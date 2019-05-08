d0382d759199365ff5d925d9dd527697a0f1dbc0
tatsuya.yanagiya <tatsuya.yanagiya@g.softbank.co.jp>
2019-05-08 17:43:21 +0900

    座標変更できるようになった

--- app/src/main/java/com/example/kaihatsuyoukanrisha/ueno_testapp/MainActivity.java
+++ app/src/main/java/com/example/kaihatsuyoukanrisha/ueno_testapp/MainActivity.java
@@ -1,16 +1,48 @@
 package com.example.kaihatsuyoukanrisha.ueno_testapp;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView earth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        earth = findViewById(R.id.testImage);
+        //WindowManager windowManager = (WindowManager)getSystemService(WINDOW_SERVICE);
+        //Display display = windowManager.getDefaultDisplay();
+        //Point point = new Point();
+        //display.getSize(point);
+        //LinearLayout layoutSize = findViewById(R.id.layout);
+        //earth.setX(point.x * 0.5f);
+        //earth.setY(point.y * 0.5f);
+        //earth.setX(layoutSize.getWidth() * 0.5f);
+        //earth.setY(layoutSze.getHeight() * 0.5f);
+
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        //earth.setImageResource(R.drawable.alien_ufo);
+        earth.setX(dm.widthPixels * 0.5f);
     }
 
     public void changeActivity(View view){
--- app/src/main/java/com/example/kaihatsuyoukanrisha/ueno_testapp/MySurfaceView.java
+++ app/src/main/java/com/example/kaihatsuyoukanrisha/ueno_testapp/MySurfaceView.java
@@ -1,10 +1,15 @@
 package com.example.kaihatsuyoukanrisha.ueno_testapp;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback, Runnable {
+
+    private Thread thread;
+    private long timer;
+
     public MySurfaceView(Context context) {
         super(context);
         getHolder().addCallback(this);
@@ -12,7 +17,9 @@ public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-
+        thread = new Thread(this);
+        thread.start();
+        timer = System.currentTimeMillis();
     }
 
     @Override
@@ -23,5 +30,20 @@ public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
 
+    }
+
+    @Override
+    public void run() {
+        while (thread != null) {
+            Canvas c = getHolder().lockCanvas();
+            if (c == null){
+                return;
+            }
+
+            //更新
+
+            //描画
+            
+        }
     }
 }
